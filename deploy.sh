e5a7f76db4d76641e8bd50d2dc9c65f131b42878
luis <luis@luis-portatil.(none)>
2018-04-08 09:58:33 +0200

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+set -x  # echo on
+
+SRC_DIR=~/src/varios
+
+echo "source $SRC_DIR/vimfiles/_vimrc" >> ~/.vimrc
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/Vundle.vim
+vim -c PluginUpdate
+
+echo "source-file $SRC_DIR/_tmux.conf" >> ~/.tmux.conf
+
+printf "[include]\n\tpath = $SRC_DIR/_gitconfig" >> ~/.gitconfig
+
+echo "--options=$SRC_DIR/_ctags" >> ~/.ctags
+
+echo "source $SRC_DIR/_bashrc" >> ~/.bashrc
+
+
+
+
