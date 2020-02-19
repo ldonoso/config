c17fbc97593dcfe978c99f45d9b4f9f670f53500
ldonoso <luisantoniod@gmail.com>
2020-02-19 10:31:46 +0000

    Use stow

--- conf/md_style.rb
+++
@@ -1,8 +0,0 @@
-all
-
-rule 'MD029', :style => :ordered
-
-# Line length
-exclude_rule 'MD013'
-# Fenced code blocks should have a language specified
-exclude_rule 'MD040'
--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,6 @@
 set -x  # echo on
 
-SRC_DIR=~/src/config
-CONF_DIR=$SRC_DIR/conf
+stow -d ./dotfiles -t ~ -S tmux bash clang ctags git haskell markdown_lint tmux vim
 
-echo "source $SRC_DIR/vimfiles/_vimrc" >> ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c PlugUpdate
-
-printf "[include]\n\tpath = $CONF_DIR/_gitconfig" >> ~/.gitconfig
-cat $CONF_DIR/_gitignore >> ~/.gitignore
-
-echo "source-file $CONF_DIR/_tmux.conf" >> ~/.tmux.conf
-echo "--options=$CONF_DIR/_ctags" >> ~/.ctags
-echo "source $CONF_DIR/_bashrc" >> ~/.bashrc
-cat $CONF_DIR/_clang-format >> ~/.clang-format
-cat $CONF_DIR/_haskeline >> ~/.haskeline
-
-cat $CONF_DIR/_ghci >> ~/.ghci
-chmod 644 ~/.ghci  # Requires go don't have write access
-
-cat $CONF_DIR/_inputrc >> ~/.inputrc
-cat $CONF_DIR/_mdlrc >> ~/.mdlrc
