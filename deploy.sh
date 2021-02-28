8bf7e23c961e0dd59b7e9e4f773563b6d051eb4f
Luis Donoso <luis.donoso@percona.com>
2021-02-28 13:00:31 +0100

    Use --dotfiles option in the command stow

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 set -x  # echo on
 
-stow -d ./dotfiles -t ~ -S tmux bash clang ctags git haskell markdown_lint tmux vim
-
-# stow ignores .gitignore
-ln -s -r ./dotfiles/git/.gitignore ~/.gitignore
+stow --dir=./dotfiles --target=$HOME --dotfiles --stow bash clang ctags git haskell markdown_lint tmux vim
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c PlugUpdate
