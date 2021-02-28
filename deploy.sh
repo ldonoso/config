84d489941d1d3c1e6f88d4fcef58d306c83fa28e
Luis Donoso <luis.donoso@percona.com>
2021-02-28 13:40:21 +0100

    Add  dot-bashrc

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 set -x  # echo on
 
-stow --dir=./dotfiles --target=$HOME --dotfiles --stow bash clang ctags git haskell markdown_lint tmux vim
+stow --dir=$HOME/src/config/dotfiles --target=$HOME --dotfiles --stow bash clang ctags git haskell markdown_lint tmux vim
+
+# We do not want too overwrite the currnt .bashrc
+echo "source $HOME/src/config/dotfiles/dot-bashrc" >> $HOME/.bashrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c PlugUpdate
