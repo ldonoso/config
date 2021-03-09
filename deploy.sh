76c7929cd20cd6a0271d99bd2ade367a02a42af4
Luis Donoso <luis.donoso@percona.com>
2021-03-09 12:47:23 +0100

    Add mysql utils

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 set -x  # echo on
 
-stow --dir=$HOME/src/config/dotfiles --target=$HOME --dotfiles --stow bash clang ctags git haskell markdown_lint tmux vim
+stow --dir=$HOME/src/config/dotfiles --target=$HOME --dotfiles --stow bash clang ctags git haskell markdown_lint tmux vim mysql
 
 # We do not want too overwrite the currnt .bashrc
 echo "source $HOME/src/config/dotfiles/dot-bashrc" >> $HOME/.bashrc
