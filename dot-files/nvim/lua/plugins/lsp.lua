ec738b7bfcaa8787768c92771c6c31872e8f96a0
Luis Donoso <luisantoniod@gmail.com>
2024-09-22 21:26:56 +0200

    nvim - lsp - add luasnip and fidget.nvim

--- dot-files/nvim/lua/plugins/lsp.lua
+++ dot-files/nvim/lua/plugins/lsp.lua
@@ -18,36 +18,95 @@ return {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
-      { 'L3MON4D3/LuaSnip' }, -- todo - configure luasnip
+      {
+        'L3MON4D3/LuaSnip',
+        build = (function()
+          -- Build Step is needed for regex support in snippets.
+          -- This step is not supported in many windows environments.
+          -- Remove the below condition to re-enable on windows.
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+        dependencies = {
+          -- `friendly-snippets` contains a variety of premade snippets.
+          --    See the README about individual language/framework/plugin snippets:
+          --    https://github.com/rafamadriz/friendly-snippets
+          -- {
+          --   'rafamadriz/friendly-snippets',
+          --   config = function()
+          --     require('luasnip.loaders.from_vscode').lazy_load()
+          --   end,
+          -- },
+        },
+      },
+      'saadparwaiz1/cmp_luasnip',
     },
     config = function()
       local cmp = require('cmp')
+      local luasnip = require 'luasnip'
+      luasnip.config.setup {}
 
       cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+
         sources = {
           { name = 'nvim_lsp' },
+          { name = 'luasnip' },
           { name = 'buffer' },
         },
 
         completion = {
-          autocomplete = false -- Invoke completion menu manually
+          autocomplete = false -- when false, trigger completion menu manually
         },
 
         mapping = cmp.mapping.preset.insert({
-          ['<CR>'] = cmp.mapping.confirm({ select = false }), -- `Enter` key to confirm completion
-          ['<C-Space>'] = cmp.mapping.complete(),             -- Ctrl+space to trigger completion menu
+          -- Select the [n]ext / [p]revious item
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+
+          -- Scroll the documentation window
           ['<C-u>'] = cmp.mapping.scroll_docs(-4),
           ['<C-d>'] = cmp.mapping.scroll_docs(4),
+
+          -- Accept ([y]es) the completion.
+          --  This will auto-import if your LSP supports it.
+          --  This will expand snippets if the LSP sent a snippet.
+          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+
+          -- Manually trigger a completion from nvim-cmp.
+          --  Generally you don't need this, because nvim-cmp will display
+          --  completions whenever it has completion options available.
+          ['<C-Space>'] = cmp.mapping.complete(),
+
+          -- Think of <c-l> as moving to the right of your snippet expansion.
+          --  So if you have a snippet that's like:
+          --  function $name($args)
+          --    $body
+          --  end
+          --
+          -- <c-l> will move you to the right of each of the expansion locations.
+          -- <c-h> is similar, except moving you backwards.
+          ['<C-l>'] = cmp.mapping(function()
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            end
+          end, { 'i', 's' }),
+
+          ['<C-h>'] = cmp.mapping(function()
+            if luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            end
+          end, { 'i', 's' }),
         }),
 
-        --- (Optional) Show source name in completion menu
+        -- (Optional) Show source name in completion menu
         formatting = require('lsp-zero').cmp_format(),
-
-        snippet = {
-          expand = function(args)
-            vim.snippet.expand(args.body)
-          end,
-        },
       })
     end
   },
@@ -62,6 +121,10 @@ return {
       { 'hrsh7th/cmp-buffer', }, -- Optional
       { 'williamboman/mason.nvim' },
       { 'williamboman/mason-lspconfig.nvim' },
+      {
+        'j-hui/fidget.nvim', -- Useful status updates for LSP
+        opts = {}            -- the same as calling `require('fidget').setup({})`
+      },
     },
     config = function()
       local lsp_zero = require('lsp-zero')
@@ -74,7 +137,7 @@ return {
 
         local opts = { buffer = bufnr }
 
-        -- some of the keympas overwrite build-in keymaps
+        -- some of the keymaps overwrite build-in keymaps
         vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
         vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
         vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
