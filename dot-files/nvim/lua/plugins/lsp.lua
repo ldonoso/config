6671b50e19ad734dcdb7df4e873ef776c77bb972
Luis Donoso <luisantoniod@gmail.com>
2024-10-02 16:34:46 +0200

    nvim: lsp - format on saving

--- dot-files/nvim/lua/plugins/lsp.lua
+++ dot-files/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,5 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+
 return {
   {
     'VonHeikemen/lsp-zero.nvim',
@@ -146,10 +148,23 @@ return {
         vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
         vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
         vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
         vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 
         vim.keymap.set('n', 'gh', '<cmd>ClangdSwitchSourceHeader<cr>', opts) -- switch .h and .cpp
+
+        if client.supports_method("textDocument/formatting") then
+          vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+
+          -- format on safe
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+          })
+        end
+
       end
 
       lsp_zero.extend_lspconfig({
