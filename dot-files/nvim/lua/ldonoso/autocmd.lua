2030fd3a28cc3d88e931b5960181e36c5e6863d9
Luis Donoso <luisantoniod@gmail.com>
2024-10-02 17:20:06 +0200

    nvim - clean code

--- dot-files/nvim/lua/ldonoso/autocmd.lua
+++ dot-files/nvim/lua/ldonoso/autocmd.lua
@@ -1,66 +1,65 @@
-local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 -- Create autocmds inside this group so they are not duplicated if autocmd.lua is loaded twice
-local LDonosoGroup = augroup(
+local augroup = vim.api.nvim_create_augroup(
     'LDonoso',
     { clear = true } -- clear existing commands if the group exists
 )
 
 -- todo: Investigate why it is necessary
 autocmd({ "BufRead", "BufNewFile", }, {
-    group = LDonosoGroup,
+    group = augroup,
     pattern = "*.md",
     command = "set syntax=markdown",
 })
 
 autocmd({ "BufRead", "BufNewFile", }, {
-    group = LDonosoGroup,
+    group = augroup,
     pattern = "*.qml",
     command = "setfiletype qmljs", -- only if filetype has not already been set
 })
 
 autocmd({ "BufRead", "BufNewFile", }, {
-    group = LDonosoGroup,
+    group = augroup,
     pattern = "*.make",
     command = "setfiletype make", -- only if filetype has not already been set
 })
 
 autocmd({ "BufRead", "BufNewFile", }, {
-    group = LDonosoGroup,
+    group = augroup,
     pattern = "*.sql",
     command = "set filetype=sqlinformix", -- overrides any filetype that has already been set
 })
 
 autocmd({ "BufRead", "BufNewFile", }, {
-    group = LDonosoGroup,
+    group = augroup,
     pattern = "*.ic",
     command = "set filetype=cpp", -- overrides any filetype that has already been set
 })
 
 -- open a quickfix item in a new window
 autocmd("Filetype", {
-    group = LDonosoGroup,
+    group = augroup,
     pattern = "qf",
     command = "nnoremap <buffer> <localleader><Enter> <C-w><Enter><C-w>L",
 })
 
 autocmd("FileType", {
-    group = LDonosoGroup,
+    group = augroup,
     pattern = { "xml", "git", }, -- if event is FileType, the pattern is the file type
     command = "set foldmethod=syntax",
 })
 
 -- last-position-jump
 autocmd({ "BufReadPost", }, {
-    group = LDonosoGroup,
+    group = augroup,
     pattern = "*",
     command = [[if line("'\"") >= 1 && line("'\"") <= line("$") && &ft !~# 'commit' | exe "normal! g`\"" | endif]],
 })
 
 -- Highlight on yank
 autocmd('TextYankPost', {
-    group = LDonosoGroup,
+    group = augroup,
     pattern = "*",
     callback = function()
         vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 1000 })
@@ -69,14 +68,14 @@ autocmd('TextYankPost', {
 
 -- Remove whitespaces
 autocmd({ "BufWritePre" }, {
-    group = LDonosoGroup,
+    group = augroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
 })
 
 -- Make panels equal size when main window is resized
 autocmd({ "VimResized" }, {
-    group = LDonosoGroup,
+    group = augroup,
     pattern = "*",
     command = [[ wincmd = ]],
 })
