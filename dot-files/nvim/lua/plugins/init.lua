4394aaaa03a298864a704b47983640bb853f3635
Luis Donoso <luisantoniod@gmail.com>
2024-10-09 09:53:07 +0200

    nvim - document colorschemes

--- dot-files/nvim/lua/plugins/init.lua
+++ dot-files/nvim/lua/plugins/init.lua
@@ -1,7 +1,6 @@
 local ft_parentheses = { "lisp", 'clojure', 'scheme', }
 
 return {
-    -- the colorscheme should be available when starting Neovim
     {
         'morhetz/gruvbox',
         enabled = false,
@@ -13,12 +12,32 @@ return {
         end,
     },
 
-    { -- treesitter and semantic highlights support
-        -- 'ellisonleao/gruvbox.nvim',
-        'sainnhe/gruvbox-material',
+    {
+        'gruvbox-community/gruvbox', -- fork of morhetz's
+        enabled = false,
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.g.gruvbox_italic = 0
+            vim.cmd([[colorscheme gruvbox]])
+        end,
+    },
+
+    {
+        'ellisonleao/gruvbox.nvim', -- port of gruvbox community to lua with treesitter and semantic highlights support!
+        enabled = false,
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd([[colorscheme gruvbox]])
+        end,
+    },
+
+    {
+        'sainnhe/gruvbox-material', -- softer contrast than morhetz's with treesitter and semantic highlights support
         enabled = true,
-        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
+        lazy = false,
+        priority = 1000,
         config = function()
             vim.cmd([[colorscheme gruvbox-material]])
         end,
