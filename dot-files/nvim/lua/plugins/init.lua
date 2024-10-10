6203ddf919e9c7d6594614e9950b88022907dbef
Luis Donoso <luisantoniod@gmail.com>
2024-10-10 06:53:04 +0200

    nvim - refactor colorschemes

--- dot-files/nvim/lua/plugins/init.lua
+++ dot-files/nvim/lua/plugins/init.lua
@@ -1,48 +1,6 @@
 local ft_parentheses = { "lisp", 'clojure', 'scheme', }
 
 return {
-    {
-        'morhetz/gruvbox',
-        enabled = false,
-        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            vim.g.gruvbox_italic = 0
-            vim.cmd([[colorscheme gruvbox]])
-        end,
-    },
-
-    {
-        'gruvbox-community/gruvbox', -- fork of morhetz's
-        enabled = false,
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.g.gruvbox_italic = 0
-            vim.cmd([[colorscheme gruvbox]])
-        end,
-    },
-
-    {
-        'ellisonleao/gruvbox.nvim', -- port of gruvbox community to lua with treesitter and semantic highlights support!
-        enabled = false,
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.cmd([[colorscheme gruvbox]])
-        end,
-    },
-
-    {
-        'sainnhe/gruvbox-material', -- softer contrast than morhetz's with treesitter and semantic highlights support
-        enabled = true,
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.cmd([[colorscheme gruvbox-material]])
-        end,
-    },
-
     'godlygeek/tabular',
     'ivanov/vim-ipython',
 
