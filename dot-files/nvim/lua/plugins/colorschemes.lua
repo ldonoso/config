1b8a6dc6b20ab4d204ef50daab7e1f2ad949aff3
Luis Donoso <luisantoniod@gmail.com>
2024-10-10 21:09:08 +0200

    nvim - disable italics

--- dot-files/nvim/lua/plugins/colorschemes.lua
+++ dot-files/nvim/lua/plugins/colorschemes.lua
@@ -16,7 +16,6 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
-            vim.g.gruvbox_italic = 0
             vim.cmd([[colorscheme gruvbox]])
         end,
     },
@@ -27,6 +26,16 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
+            require("gruvbox").setup({
+                italic = {
+                    strings = false,
+                    emphasis = false,
+                    comments = false,
+                    operators = false,
+                    folds = false,
+                },
+            })
+
             vim.cmd([[colorscheme gruvbox]])
         end,
     },
